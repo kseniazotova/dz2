0beb13749693701f670ffd6675968cadb3dcef25
ksenia <zotova508@mail.ru>
2023-06-19 13:24:05 +0300

    Добавила решение к задаче 13 и 15

---
+++ dz2/n13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+if  (num < 100) Console.WriteLine($"Третьей цифры в числе {num} нет");
+while (num > 1000) 
+{
+num = num / 10;
+ }
+int num2 = num % 10;
+Console.WriteLine($"Третья цифра в числе -> {num2}"); 
---
+++ dz2/n15/Program.cs
@@ -0,0 +1,6 @@
+﻿ //Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. 6 -> да
+Console.WriteLine("Введите число дня недели");
+int num = int.Parse(Console.ReadLine());
+if ((num == 7) || (num == 6)) Console.WriteLine("Выходной"); 
+else if ((num < 1) || (num > 7 )) Console.WriteLine("Такого дня недели нет");
+else if ((num > 0) || (num < 6 )) Console.WriteLine("Будний день");
